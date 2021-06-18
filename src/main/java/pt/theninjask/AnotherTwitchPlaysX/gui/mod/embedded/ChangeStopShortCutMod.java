66bb377c34e50e1297d4d82c9925f2a9f4f4cdc0
Tiago Bica <42300153+TheNinjask@users.noreply.github.com>
2021-06-18 21:00:46 +0100

    Update ChangeStopShortCutMod.java
    *Fixed an embedded mod.

--- src/main/java/pt/theninjask/AnotherTwitchPlaysX/gui/mod/embedded/ChangeStopShortCutMod.java
+++ src/main/java/pt/theninjask/AnotherTwitchPlaysX/gui/mod/embedded/ChangeStopShortCutMod.java
@@ -23,58 +23,31 @@ public class ChangeStopShortCutMod extends ATPXMod {
 	private JLabel press;
 
 	private JPanel panel;
-	
+
 	public ChangeStopShortCutMod() {
 		panel = new JPanel(new GridLayout(2, 1));
-		try {
-			panel.setBackground(Constants.TWITCH_COLOR);
-			press = new JLabel(
-					String.format("Press a key (Current: %s)", NativeKeyEvent.getKeyText(Constants.stopKey)));
-			press.setForeground(Constants.TWITCH_COLOR_COMPLEMENT);
-			press.setHorizontalAlignment(JLabel.CENTER);
-			panel.add(press);
-			panel.setFocusable(true);
-			GlobalScreen.registerNativeHook();
-			GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
+		panel.setBackground(Constants.TWITCH_COLOR);
+		press = new JLabel(String.format("Press a key (Current: %s)", NativeKeyEvent.getKeyText(Constants.stopKey)));
+		press.setForeground(Constants.TWITCH_COLOR_COMPLEMENT);
+		press.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(press);
+		panel.setFocusable(true);
+		JButton ok = new JButton("Confirm");
+		ok.addActionListener(l -> {
+			try {
+				GlobalScreen.unregisterNativeHook();
 
-				@Override
-				public void nativeKeyTyped(NativeKeyEvent e) {
-					// DO NOTHING
-				}
+				Constants.STRING_TO_KEYCODE.remove(NativeKeyEvent.getKeyText(Constants.stopKey));
 
-				@Override
-				public void nativeKeyReleased(NativeKeyEvent e) {
-					// DO NOTHING
-				}
-
-				@Override
-				public void nativeKeyPressed(NativeKeyEvent e) {
-					press.setText(
-							String.format("Press a key (Current: %s)", NativeKeyEvent.getKeyText(e.getKeyCode())));
-					Constants.stopKey = e.getKeyCode();
-				}
-			});
-			JButton ok = new JButton("Confirm");
-			ok.addActionListener(l -> {
-				try {
-					GlobalScreen.unregisterNativeHook();
-
-					Constants.STRING_TO_KEYCODE.remove(NativeKeyEvent.getKeyText(Constants.stopKey));
-
-					MainFrame.replacePanel(MainMenuPanel.getInstance());
-				} catch (NativeHookException e1) {
-					Constants.showExceptionDialog(e1);
-					MainMenuPanel.getInstance().getModButton().setEnabled(false);
-					MainFrame.replacePanel(MainMenuPanel.getInstance());
-				}
-			});
-			panel.add(ok);
-			MainMenuPanel.getInstance().getModButton().setText("Change Stop Key");
-		} catch (NativeHookException e) {
-			Constants.showExceptionDialog(e);
-			MainMenuPanel.getInstance().getModButton().setEnabled(false);
-			MainFrame.replacePanel(MainMenuPanel.getInstance());
-		}
+				MainFrame.replacePanel(MainMenuPanel.getInstance());
+			} catch (NativeHookException e1) {
+				Constants.showExceptionDialog(e1);
+				MainMenuPanel.getInstance().getModButton().setEnabled(false);
+				MainFrame.replacePanel(MainMenuPanel.getInstance());
+			}
+		});
+		panel.add(ok);
+		MainMenuPanel.getInstance().getModButton().setText("Change Stop Key");
 	}
 
 	@Override
